adfc46d5031dec2a456cf95ee43b9a076c95b01c
hansoleorka <hans.ole.orka@gmail.com>
2015-01-08 13:48:17 +0100

    tests

--- R/bbox2polygon.R
+++ R/bbox2polygon.R
@@ -8,11 +8,16 @@
 #' @details Check function...  
 #' @author Hans Ole {\O}rka \email{hans.ole.orka@@gmail.org}
 bbox2polygon <- function(obj,ID=names(obj)[1]){
-  bb <- bbox(obj)
+  if(class(obj) == "matrix) {
+	bb <- obj
+     crs <- NA 
+  }	
+  if(attr(class(outline),"package") == "sp") {
+	bb <- bbox(obj)
+	crs <- CRS(proj4string(obj)))
+  }
   x <- c(bb[1,1],bb[1,2],bb[1,2],bb[1,1],bb[1,1])
   y <- c(bb[2,1],bb[2,1],bb[2,2],bb[2,2],bb[2,1]) 
-  SpatialPolygons(list(Polygons(list(Polygon(cbind(x,y))),ID=ID)),proj4string=CRS(proj4string(obj)))
+  SpatialPolygons(list(Polygons(list(Polygon(cbind(x,y))),ID=ID)),proj4string=crs)
 }
 
-library(devtools)
-install_github("hansoleorka/myR")
